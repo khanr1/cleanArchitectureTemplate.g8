ebc8fb50b33f80abf6988a3495d346e68786bef1
khanr1 <raphael.khan@gmail.com>
2024-03-08 22:52:37 +0200

    renaming folder

--- src/main/g8/05-main/src/test/scala/$package__lower,package,packaged$/$name__lower,word$/ExampleSuite.scala
+++ src/main/g8/05-main/src/test/scala/$package__lower,package,packaged$/$name__lower,word$/ExampleSuite.scala
@@ -5,7 +5,7 @@ import cats.effect.IO
 import weaver._
 
 object ExampleSuite extends SimpleIOSuite {
-  test("make sure IO computes the right result") {
+  test("make sure IO computes the right result..") {
     IO.pure(1).map(_ + 2) map { result =>
       expect.eql(result, 3)
     }
